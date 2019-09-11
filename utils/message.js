a68b0375f1a5a5a568501a92dd7d258a19f8439b
vikash <vikashraj144@gmail.com>
2019-09-11 15:49:37 +0530

    utills message added

---
+++ utils/message.js
@@ -0,0 +1,12 @@
+module.exports = {
+    success:{
+        LOGIN:'Login successful',
+        REGISTER:'Register successful',
+    },
+    error:{
+        INVALID_LOGIN:'Invalid login credentials',
+        UNABLE_REGISTER:'Unable to register',
+        USER_ALLREADY_EXIST:'User already exist',
+    },
+
+};
