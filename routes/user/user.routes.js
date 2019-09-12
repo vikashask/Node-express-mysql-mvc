365358318cfad263d39849f01f82ae286f784fb2
vikash <vikashraj144@gmail.com>
2019-09-12 14:46:47 +0530

    routes added

--- routes/user/user.routes.js
+++ routes/user/user.routes.js
@@ -5,5 +5,7 @@ const userController = require('../../controllers/userController');
 router.get('/', userController.getUsers);
 router.post('/login', userController.login);
 router.post('/register', userController.register);
+router.post('/forgot-password', userController.forgotPassword);
+router.post('/logout', userController.register);
 
 module.exports = router;
