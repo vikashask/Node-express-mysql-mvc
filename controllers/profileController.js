aa1caf27698d2cff08345ef2616e1cdf4ff32916
vikash <vikashraj144@gmail.com>
2019-09-12 16:09:30 +0530

    updateProfile api

--- controllers/profileController.js
+++ controllers/profileController.js
@@ -4,7 +4,10 @@ var message = require('../utils/message')
 
 module.exports.getProfile = async (req, res) => {
   try {
-    var data = await knex.where({email_id:req.body.email_id,isActive:1}).select('email_id','profile_pic','first_name','last_name','phone_number').table('user');
+    var data = await knex.where({
+      email_id: req.body.email_id,
+      isActive: 1
+    }).select('email_id', 'profile_pic', 'first_name', 'last_name', 'phone_number').table('user');
     console.log("---", JSON.parse(JSON.stringify(data))[0]);
     return res.json({
       response: JSON.parse(JSON.stringify(data))[0]
@@ -17,44 +20,54 @@ module.exports.getProfile = async (req, res) => {
 module.exports.updateProfile = async (req, res) => {
   try {
     let userData = await checkUserExist(req.body.email_id);
-    if(userData){
-      console.log("userData",userData);
-      
+    if (userData) {
+      if (md5(req.body.password) === userData[0].password) {
+        let userProfile = {
+          first_name: req.body.first_name,
+          last_name: req.body.last_name,
+          phone_number: req.body.phone_number,
+          profile_pic: req.body.profile_pic,
+        }
+        var data = await knex('user').where({
+          email_id: req.body.email_id
+        }).update(userProfile);
+        console.log("data---", data);
+        if (data > 0) {
+          return res.json({
+            message: message.success.PROFILE_UPDATED
+          });
+        } else {
+          return res.json({
+            response: message.error.SOMETHING_WRONG
+          });
+        }
+      } else {
+        return res.json({
+          message: message.error.PASSWORD_NOT_MATCHED
+        });
+      }
+    } else {
       return res.json({
-        message:message.error.USER_ALLREADY_EXIST
+        message: message.error.USER_NOT_EXIST
       });
-    }else{
-      let userProfile = {
-        first_name: req.body.first_name,
-        last_name: req.body.last_name,
-        phone_number: req.body.phone_number,
-        profile_pic: req.body.profile_pic,
-      }
-      var data = await knex('user').where({email_id:req.body.email_id}).update(userProfile);
-      if(data.length>0){
-        return res.json({
-          message:message.success.REGISTER
-        });
-      }else{
-        return res.json({
-          response: message.error.UNABLE_REGISTER
-        });
-      }
     }
   } catch (e) {
     console.log(e)
   }
 }
 
-module.exports.logout = async (req,res) =>{
+module.exports.logout = async (req, res) => {
   try {
-    var userDetails = await knex('user').where({email_id:req.body.email_id,isActive:1})
-    if(userDetails.length>0){
-      console.log("data",JSON.parse(JSON.stringify(userDetails)));
+    var userDetails = await knex('user').where({
+      email_id: req.body.email_id,
+      isActive: 1
+    })
+    if (userDetails.length > 0) {
+      console.log("data", JSON.parse(JSON.stringify(userDetails)));
       return res.json({
         response: message.success.FORGOT_PASSWORD_SENT
       });
-    }else{
+    } else {
       return res.json({
         response: message.error.USER_NOT_EXIST
       });
@@ -64,12 +77,15 @@ module.exports.logout = async (req,res) =>{
   }
 }
 
-var checkUserExist = async (email_id) =>{
+var checkUserExist = async (email_id) => {
   try {
-    var data = await knex('user').where({email_id:email_id,isActive:1})
-    if(data.length>0){
+    var data = await knex('user').where({
+      email_id: email_id,
+      isActive: 1
+    })
+    if (data.length > 0) {
       return JSON.parse(JSON.stringify(data));
-    }else{
+    } else {
       return false;
     }
   } catch (e) {
