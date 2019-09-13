29ef74c75a7d2304698dbd6941b7e8082fc77d18
vikash <vikashraj144@gmail.com>
2019-09-13 12:49:12 +0530

    brand routes defined

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,9 @@ const router = express.Router();
 
 const userRoutes = require('./user/user.routes');
 const profileRoutes = require('./user/profile.routes');
+const brandRoutes = require('./brand.routes');
 
 router.use('/user', userRoutes);
 router.use('/profile', profileRoutes);
+router.use('/brand', brandRoutes);
 module.exports = router;
