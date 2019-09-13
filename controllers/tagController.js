d652b9c56fd6a8ca8b3588b69ad6c05c02f7d0cd
vikash <vikashraj144@gmail.com>
2019-09-13 17:52:42 +0530

    tag api

--- controllers/tagController.js
+++ controllers/tagController.js
@@ -46,6 +46,36 @@ module.exports.addTag = async (req, res) => {
   }
 }
 
+module.exports.deleteTag = async (req, res) => {
+  try {
+    let ids = req.params.ids.split(',');
+    console.log("id",req.params);
+    
+    // console.log("id",req);
+    process.exit()
+    let tagInsertData = await knex('tags').where({
+      id: req.body.id
+    }).del();
+    console.log(tagInsertData);
+    
+    if (tagInsertData > 0) {
+      return res.json({
+        response: req.body,
+        message: message.success.TAG_UPDATE
+      })
+    } else {
+      return res.json({
+        response: message.error.SOMETHING_WRONG
+      })
+    }
+  } catch (error) {
+    console.log(error);
+    return res.json({
+      response: error
+    })
+  }
+}
+
 module.exports.updateTag = async (req, res) => {
   try {
     let tagInsertData = await knex('tags').where({
--- routes/tag.routes.js
+++ routes/tag.routes.js
@@ -5,5 +5,6 @@ const tagController = require('../controllers/tagController');
 router.get('/get-tag', tagController.getTag);
 router.post('/add-tag', tagController.addTag);
 router.put('/update-tag', tagController.updateTag);
+router.delete('/delete-tag/:ids', tagController.deleteTag);
 
 module.exports = router;
