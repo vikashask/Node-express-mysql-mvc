8179af5a3de5c96ef36127eb843800b4715845f9
vikash <vikashraj144@gmail.com>
2019-09-16 17:40:37 +0530

    brand api updated

--- controllers/brandController.js
+++ controllers/brandController.js
@@ -2,21 +2,30 @@ var knex = require('../config/knex');
 var md5 = require('md5');
 var message = require('../utils/message')
 
-module.exports.getBrand = async (req,res) => {
-    try {
-        var brandData = await knex('brand').where({
-            isActive: 1
-          })
-        if(brandData.length>0){
-            return res.json({
-              response: JSON.parse(JSON.stringify(brandData))
-            })        
-        }else{
-            return res.json({
-              response: message.error.BRAND_NOT_FOUND
-            })        
-        }
-    } catch (error) {
-        console.log(error)        
+module.exports.getBrand = async (req, res) => {
+  try {
+    let brandData;
+    if (req.params.id) {
+      brandData = await knex('brand').where({
+        id: req.params.id,
+        isActive: 1
+      });
+    } else {
+      brandData = await knex('brand')
+        .where({
+          isActive: 1
+        });
     }
+    if (brandData.length > 0) {
+      return res.json({
+        response: JSON.parse(JSON.stringify(brandData))
+      })
+    } else {
+      return res.json({
+        response: message.error.BRAND_NOT_FOUND
+      })
+    }
+  } catch (error) {
+    console.log(error)
+  }
 }
--- routes/brand.routes.js
+++ routes/brand.routes.js
@@ -2,6 +2,6 @@ const express = require('express');
 const router = express.Router();
 
 const brandController = require('../controllers/brandController');
-router.get('/get-brand', brandController.getBrand);
+router.get('/get-brand/:id?', brandController.getBrand);
 
 module.exports = router;
