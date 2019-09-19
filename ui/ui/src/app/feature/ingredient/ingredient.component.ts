02ddd4845a3ad6d8aed64da417c0d2bf2eddcca7
Sanjay choudhary <MPLLC\sanjay.choudhary@a1-p-d04.mpllc.com>
2019-09-19 19:55:44 +0530

    Ingredient page html

--- ui/ui/src/app/feature/ingredient/ingredient.component.ts
+++ ui/ui/src/app/feature/ingredient/ingredient.component.ts
@@ -23,7 +23,7 @@ export class IngredientComponent implements OnInit {
 
   }
   AddIngredient() {
-
+    console.log(this.ingredientForm.value);
   }
 
 
--- ui/ui/src/app/feature/tag/tag.component.ts
+++ ui/ui/src/app/feature/tag/tag.component.ts
@@ -19,6 +19,7 @@ export class TagComponent implements OnInit {
     })
   }
   addTag() {
+    console.log(this.tagForm.value);
 
   }
 }
