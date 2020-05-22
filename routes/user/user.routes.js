202c3c8f3c909e7d10676c5532a486c95b854f5a
vikash <vikashraj144@gmail.com>
2020-05-22 20:16:32 +0530

    api route added

--- routes/user/user.routes.js
+++ routes/user/user.routes.js
@@ -1,10 +1,11 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
-const userController = require('../../controllers/userController');
-router.get('/', userController.getUsers);
-router.post('/login', userController.login);
-router.post('/register', userController.register);
-router.post('/forgot-password', userController.forgotPassword);
+const userController = require("../../controllers/userController");
+router.get("/", userController.getUsers);
+router.post("/login", userController.login);
+router.post("/register", userController.register);
+router.post("/forgot-password", userController.forgotPassword);
+router.get("/stream-video", userController.streamVideo);
 
 module.exports = router;
